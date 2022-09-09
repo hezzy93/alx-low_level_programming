d34b42dc7108e9d8cf1b376a3bae2fb33b9d399f
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-09 15:41:20 -0700

    Assign random number and print the last number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: Get the last digit of a random number and print the number
+ *
+ * and if it is greater than 5,equal to 0 andless than 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n, y;
+	srand(time(0));
+	n = rand() - RAND_MAX/2;
+	y = n % 10;
+if (y > 5)
+
+	printf("Last digit of %d is %d and is greater than 5\n", n, y)
+
+else if (y < 6)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y)
+
+else
+	printf("Last digit of %d is %d and is 0\n", n, y)
+
+
+return (0)
+}
