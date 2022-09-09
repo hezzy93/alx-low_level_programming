684248fb4839aa0344b2d46d09c3cb3e519dae1c
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-09 14:16:03 -0700

    eddited the conditonal statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 
 if (n > 0)
 {
-	printf("n is positive \n");
+	printf("%d \n", n "is positive \n");
 }
 else if (n < 0)
 {
-	printf("n is negative \n");
+	printf("%d \n", n "is negative \n");
 }
 else
 {
-	printf("n is zero \n");
+	printf("%d \n", "n is zero \n");
 }
 return (0);
 }
