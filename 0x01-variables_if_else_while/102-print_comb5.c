a416f1bbdfab0ab34d945416200cf22b4e078751
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-09 19:45:51 -0700

    kdf

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,14 +4,19 @@
  * Return: 0
  */
 int main(void)
-int num1, num2;
-for (num1 = 0; num1 <= 98; num1++)
-for (num2 = num1 + 1; num2 <= 99; num2++)
-putchar((num1 / 10) + '0');
-putchar((num1 % 10) + '0');
-putchar(' ');								putchar((num2 / 10) + '0');
-putchar((num2 % 10) + '0');
-if (num1 == 98 && num2 == 99)
+{
+int a, b, c, d;
+for (a = '0'; a <= '9'; a++)
+for (b = '0'; b <= '9'; b++)
+for (c = '0'; c <= '9'; c++)
+for (d = '0'; d <= '9'; d++)
+if ((a < c) || (a == c && b < d))
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+if (!(a == '9' && b == '8'))
 putchar(',');
 putchar(' ');
 putchar('\n');
