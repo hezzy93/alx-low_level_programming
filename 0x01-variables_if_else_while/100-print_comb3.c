002035f22fe9c22bb03a4086581dabb7b7845369
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-12 10:52:07 -0700

    hdfjkdfj

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,21 @@ int main(void)
 int num1;
 int num2;
 for (num1 = '0'; num1 < '9'; num1++)
+{
 for (num2 = num1 + 1; num2 <= '9'; num2++)
+{
 if (num2 !=num1)
+{
 putchar(num1);
 putchar(num2);
 if (num1 == '8' && num2 == '9')
 {
 continue;
+}
 putchar(',');
 putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
