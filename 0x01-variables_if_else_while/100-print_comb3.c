e506ab853431d4a50ac5200129ebfc9fd1d89cad
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-12 10:46:08 -0700

    jdhfiu

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,7 @@ int num1;
 int num2;
 for (num1 = '0'; num1 < '9'; num1++)
 for (num2 = num1 + 1; num2 <= '9'; num2++)
-{
 if (num2 !=num1)
-{
 putchar(num1);
 putchar(num2);
 if (num1 == '8' && num2 == '9')
@@ -20,7 +18,6 @@ if (num1 == '8' && num2 == '9')
 continue;
 putchar(',');
 putchar(' ');
-}
 }
 putchar('\n');
 return (0);
