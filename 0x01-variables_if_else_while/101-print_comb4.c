871a149c9d2d67c22cbfb68d4d74a88a8305c6e0
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-12 07:49:34 -0700

    task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,9 +16,9 @@ putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 putchar((digit3 % 10) + '0');
 if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+}
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
