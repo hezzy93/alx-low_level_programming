44db2a0167ec614049abadd0cb34320f0bd3d0ba
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-13 13:21:03 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stidio.h>
+/**
+ * main - Entry point
+ * Description: Get a random number and print the number
+ * Return:0
+ */
+int main(void)
+{
+char ch = '_putchar';
+putchar(ch);
+return (0);
+}
