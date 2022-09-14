9344b04ac780046c2fe5b660361c71cb4da0b1fd
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-13 18:52:30 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sing - function to check for a sign of a number
+ * print_sign - function to check for a sign of a number
  * @n: is the int that will use for the argument of the function
  * Description: The best code writing ever, keep moving.
  * Return: 0
