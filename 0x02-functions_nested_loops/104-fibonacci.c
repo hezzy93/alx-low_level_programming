f04e0636d76d9af3b1ca9438ca1c486c94469940
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-14 13:52:54 -0700

    task14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
+int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
 unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
@@ -39,8 +40,9 @@ printf(", ");
 
 fib1_half1 = fib2_half1;
 fib1_half2 = fib2_half2;
-fib2_half1 = half1
+fib2_half1 = half1;
 fib2_half2 = half2;
+
 }
 printf("\n");
 return (0);
