fe852cf55e227fc8fd2c9b6ceb4e96fbe696e69a
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-13 17:07:00 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,6 +5,7 @@
 void times_table(void)
 {
 	int num, mult, prod;
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
@@ -13,7 +14,7 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			prod = num *mult;
+			prod = num * mult;
 
 			if (prod <= 9)
 				_putchar(' ');
