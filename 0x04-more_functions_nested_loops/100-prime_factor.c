35cfb5ba4a2f5ddbc4ebe4c56b7e3e63cf0baaf3
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-15 14:54:36 -0700

    hsf

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * main- prints the largest prime factor
+ * main-prints the largest prime factor
  * of a number
  * Return: returns 0
  */
-
 int main(void)
 {
 	long number = 612852475143;
 	int inc;
 
-	while (inc++ < number /2)
+	while (inc++ < number / 2)
 	{
 	if (number % inc == 0)
 	{
-		number /=2;
+		number /= 2;
 		continue;
 	}
 
@@ -27,5 +26,4 @@ int main(void)
 	}
 	printf("%ld\n", number);
 	return (0);
-
 }
