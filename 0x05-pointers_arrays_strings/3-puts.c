94e2c5ed38953940921165fac4b76f697adf329e
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-19 15:38:11 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,10 +2,9 @@
 #include "main.h"
 
 /**
- * @_puts - prints a string
- * @str: string to print
- * Description: prints a string
- * On success: returns no error
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
  */
 
 void _puts(char *str)
