5d3b33b4a1ecb3e064965bca6638553d1f1705fd
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-22 14:46:58 -0700

    task 4

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,20 +3,19 @@
 /**
  * string_toupper - changes all lowercase letters to uppercase
  * @str: string to be changed
- *
  * Return: address to thr string
  */
 
-char *string_toupper(char *);
+char *string_toupper(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
-
-		i++;
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
 	}
 	return (str);
 }
