c95c79e301f4e6ac091749680074b7a953bcc7b6
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-22 17:30:53 -0700

    task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,24 +7,29 @@
 
 void print_number(int n)
 {
-	/*Declaring variables*/
-	int count = 0, pow = 1;
-	unsigned int num = n;
+	unsigned int m, d, count;
 
-	if (n < 0) /*Evaluate this condition*/
+	if (n < 0)
 	{
-		_putchar('-');
-		num = -n;
+		_putchar(45);
+		m = n * -1;
 	}
-	while (n !=0)
+	else
 	{
-		n /= 10;
-		count++;
+		m = n;
+	}
 
-	}
-	while (count > 1)
+	d = m;
+	count = 1:
+
+		while (d >)
+		{
+			d /=10;
+			count *= 10;
+		}
+
+	for (; count >= 1; count /= 10)
 	{
-		_putchar(num / pow % 10 + '0');
-		pow /= 10;
+		_putchar(((m / count) % 10) + 48);
 	}
 }
