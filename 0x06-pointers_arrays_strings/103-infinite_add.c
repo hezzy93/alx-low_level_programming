24ddb728eeae611873032012454692f8fa51cd86
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-22 17:21:55 -0700

    eduiler

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,7 +9,6 @@
  * @size_r: buffer size
  * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
@@ -24,11 +23,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
-		if (i >= 0)
+	if (i >= 0)
 			n += n1[i] - '0';
-		if (j >= 0)
+		
+	if (j >= 0)
 			n += n2[j] -'0';
-		if (i < 0 && j < 0 && n == 0)
+	if (i < 0 && j < 0 && n == 0)
 		{
 			break;
 		}
