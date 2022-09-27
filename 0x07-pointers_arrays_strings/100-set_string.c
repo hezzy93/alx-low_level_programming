cfeeb1621303de6a3ab6962251380cbb1077d349
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-26 17:29:13 -0700

    all remaining tasks

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,7 +2,7 @@
 
 /**
  * set_string - sets the value of a pointr to a char
- * @s: pointer to change 
+ * @s: pointer to change
  * @to: string to change pointer to
  * Return: void
  */
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,4 @@
 #include "main.h"
- 
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string to check
@@ -10,6 +9,7 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
+
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
