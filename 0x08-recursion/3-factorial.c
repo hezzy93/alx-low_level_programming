24a03aaf521134f526c872262c5be7db227c26d5
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-28 13:36:32 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,11 @@
  * Return: an integer
  */
 
-nt factorial(int n)
+int factorial(int n)
 {
 	if (n < 0)
-		return;
+		return (-1);
 	if (n <= 1)
 		return (1);
-	return (n * factorial(n - 1);
+	return (n * factorial(n - 1));
 }
