0c9a180dee087eb748426144a0310417ebd93115
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-30 16:32:40 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -this is a fuction that sum positve numbers
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num, i, j;
+
+	if (argc == 1)
+		printf("0\n");
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+	}
+	for (k = 0; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", sum)
+		return (0)
+}
