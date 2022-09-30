f5ab8999f93530206c1fb438f24e799249d5d4db
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-30 15:59:25 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - this is a function to print its name
  * @argc: argc parameter
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	int result = 0, num1, num2;
 
-	if (argc !=3)
+	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
