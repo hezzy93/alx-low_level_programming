1e25e03c660702a5bbd4cb0d76c9e0b652d3594c
Hezekiah <hezekiahajomewo@gmail.com>
2022-09-30 17:03:48 -0700

    task 44

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-if (argv[i][j] > '9' || argv[i][j] < '0')
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 {
 			printf("%s\n", "Error");
 			return (1);
