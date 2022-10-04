c279034be039791fd2f30b7cef206cfe18111d08
Hezekiah <hezekiahajomewo@gmail.com>
2022-10-04 13:45:56 -0700

    task 1 - 6

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#iclude "main.h"
+
+/**
+ * _strdup - function returns a pointer to a new string
+ * @str: string to be copied
+ * Return: NULL in case of error, pointer to alllocated space
+ */
+
+char *_strdup(char *str)
+{
+	char *cpy;
+	int index, len;
+
+	if (str == NULL)
+	return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy =malloc(sizeof(char) * (len + 1));
+
+	if (cpy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+
+	cpy[len] = '\0';
+
+	return (cpy);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * free_grid - function that fress a 2D grid
+ * @height: height of the array
+ * Return: returns void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+		free(grid[i];
+				free(grid);
+				}
