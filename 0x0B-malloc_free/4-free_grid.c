579fb6592579ab1cd78bd7ad8c2504c52d33eead
Hezekiah <hezekiahajomewo@gmail.com>
2022-10-04 17:27:07 -0700

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,12 +3,14 @@
 /**
  * free_grid - function that fress a 2D grid
  * @height: height of the array
+ * @grid: grid of the array
  * Return: returns void
  */
 
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 		free(grid[i]);
 				free(grid);
