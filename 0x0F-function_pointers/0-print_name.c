b42786ce5d30104f74e039719bef1a0b541aeff7
Hezekiah <hezekiahajomewo@gmail.com>
2022-10-12 11:18:37 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name -as_is - prints a name as is
- * @name: name of the person
+ * print_name - Print a name from a function pointer
+ * @name: char string
  * @f: function pointer that takes a string argument
- * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
